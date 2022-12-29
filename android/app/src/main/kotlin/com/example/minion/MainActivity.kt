d82de875f372f2ed43bd158181f9dad8b7effc04
LIBINLUVIS <libinluvis10@gmail.com>
2022-12-30 01:02:20 +0530

    the first commit

---
+++ android/app/src/main/kotlin/com/example/minion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.minion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,311 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'mqtt_client.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.,
+      // ),
+      theme: Theme.of(context).copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: const Color(0xFF784CEF),
+        ),
+      ),
+      home: const Home(title: 'Minion'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late MqttClient client;
+  var topic = "minion/childsectiondata";
+
+  void _publish(String message) {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString('Hello from flutter_client');
+    client.publishMessage(topic, MqttQos.atLeastOnce, builder.payload);
+  }
+  var data='';
+  void alert(){
+    if(data=='200'){
+      showAlertDialog(BuildContext context) {
+        // set up the button
+        Widget okButton = TextButton(
+          child: Text("OK"),
+          onPressed: () { },
+        );
+
+        // set up the AlertDialog
+        AlertDialog alert = AlertDialog(
+          title: Text("Minion"),
+          content: Text("Connected"),
+          actions: [
+            okButton,
+          ],
+        );
+        // show the dialog
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return alert;
+          },
+        );
+      }
+    }
+  }
+  Future<MqttClient> connect() async {
+    MqttServerClient client =
+    MqttServerClient.withPort('broker.emqx.io', 'flutter_client', 1883);
+    client.logging(on: true);
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    client.onUnsubscribed = onUnsubscribed;
+    client.onSubscribed = onSubscribed;
+    client.onSubscribeFail = onSubscribeFail;
+    client.pongCallback = pong;
+
+    final connMess = MqttConnectMessage()
+        .withClientIdentifier("flutter_client")
+        .authenticateAs("test", "test")
+        .keepAliveFor(60)
+        .withWillTopic('willtopic')
+        .withWillMessage('My Will message')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+    client.connectionMessage = connMess;
+    try {
+      print('Connecting');
+      await client.connect();
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    if (client.connectionStatus.state == MqttConnectionState.connected) {
+      print('EMQX client connected');
+      client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+        final MqttPublishMessage message = c[0].payload as MqttPublishMessage;
+        final payload =
+        MqttPublishPayload.bytesToStringAsString(message.payload.message);
+
+        print('Received message:$payload from topic: ${c[0].topic}>');
+
+        //  data=payload;
+        setState(() {
+          data=payload;
+        });
+
+      });
+
+      client.published.listen((MqttPublishMessage message) {
+        print('published');
+        final payload =
+        MqttPublishPayload.bytesToStringAsString(message.payload.message);
+        print(
+            'Published message: $payload to topic: ${message.variableHeader.topicName}');
+      });
+    } else {
+      print(
+          'EMQX client connection failed - disconnecting, status is ${client.connectionStatus}');
+      client.disconnect();
+      exit(-1);
+    }
+
+    return client;
+  }
+
+  void onConnected() {
+    print('Connected');
+  }
+
+  void onDisconnected() {
+    print('Disconnected');
+  }
+
+  void onSubscribed(String topic) {
+    print('Subscribed topic: $topic');
+  }
+
+  void onSubscribeFail(String topic) {
+    print('Failed to subscribe topic: $topic');
+  }
+
+  void onUnsubscribed(String topic) {
+    print('Unsubscribed topic: $topic');
+  }
+
+  void pong() {
+    print('Ping response client callback invoked');
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children:[
+            const SizedBox(height: 30,width: 20,),
+            const Text("Child status",style: TextStyle(fontSize: 30,color: Color(0xFF10A19D))),
+            const SizedBox(height: 20,width: 20,),
+            Container(
+              margin:const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    child: const Text('Connect',style: TextStyle(fontSize:20),),
+                    onPressed: () => {
+                      connect().then((value) {
+                        client = value;
+                        client.subscribe(topic, MqttQos.atLeastOnce);
+                        alert();
+
+                      })
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 60,width: 30,),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children:  [
+                data=='300'?
+                const Icon(Icons.escalator_warning,
+                  color: Colors.green,
+                  size: 150,
+                ):const Text(''),
+                data=='300'?
+                const Text("Safe distance",style: TextStyle(fontSize: 20,color: Color(0xFF10A19D)),):
+                const Text(""),
+
+                data=='500'?
+                const Icon(Icons.escalator_warning,
+                  color: Colors.red,
+                  size: 150,
+                ):const Text(''),
+                data=='500'?
+                const Text("Alert",style: TextStyle(fontSize: 20,color: Color(0xFF10A19D)),):
+                const Text(""),
+                data==''?
+                const Text("Please click connect to view the status",
+                  style: TextStyle(fontSize: 20,color: Color(0xFF10A19D)),):
+                const Text('')
+              ],
+
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+// HOME PAGE WIDGET
+
+class Home extends StatefulWidget{
+  const Home({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with WidgetsBindingObserver{
+  late MqttClient client;
+  var topic = "minion/childsectiondata";
+  @override
+  initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.inactive ||
+        state == AppLifecycleState.detached) return;
+
+    final isBackground = state == AppLifecycleState.paused;
+
+    if (isBackground) {
+      connect().then((value) {
+        client = value;
+        client.unsubscribe(topic);
+        client.disconnect();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed:(){
+             Navigator.push(context, MaterialPageRoute(builder: (context)=>const
+             MyHomePage(title: 'Minion') ));
+            }, child: const Text("Child status",style:
+            TextStyle(fontSize:20 ),)),
+            const SizedBox(height: 30,width: 30,),
+            ElevatedButton(onPressed:(){
+
+            }, child: const Text("Child tracking",
+            style: TextStyle(fontSize: 20 ),)),
+            const SizedBox(height: 30,width: 30,),
+            ElevatedButton(onPressed:(){
+
+            }, child:const Text("Child health",style: TextStyle(fontSize: 20),))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
